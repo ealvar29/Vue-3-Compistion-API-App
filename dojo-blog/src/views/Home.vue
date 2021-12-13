ae9e8e30f1fc62f08d28e7bb116cd6e79382330f
Eduardo <ealvarez@carminaticonsulting.com>
2021-12-13 16:32:19 -0600

    Vue reactive vs refs

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinja">Update Ninja One</button>
+
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age}}</p>
+    <button @click="updateNinjaTwo">Update Ninja Two</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { ref, reactive } from 'vue';
+
+export default {
+  name: 'Home',
+  setup() {
+    const ninjaOne = ref({ name: 'mario', age: 30 })
+
+    const ninjaTwo = reactive({ name: 'Yoshi', age: 35 })
+
+    const updateNinja = () => {
+      ninjaOne.value.age = 40
+    }
+
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 50
+    }
+
+    
+    return { ninjaOne, updateNinja, ninjaTwo, updateNinjaTwo}
+  },
+}
+</script>
