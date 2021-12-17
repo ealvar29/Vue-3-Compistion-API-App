f2f722da6138823d3b40cb3efb3246e3e7432beb
Eduardo <ealvarez@carminaticonsulting.com>
2021-12-16 21:01:48 -0600

    home fixed

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,40 +1,40 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="home">
     <div v-if="error">{{ error }}</div>
     <div v-if="posts.length">
-          <PostList :posts="posts"/>
+      <PostList :posts="posts" />
     </div>
     <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import { ref, computed } from 'vue';
-import PostList from '../components/PostList.vue';
-
+import { ref } from 'vue'
+// component imports
+import PostList from '../components/PostList.vue'
 export default {
   name: 'Home',
   components: { PostList },
-  setup() {
-    const posts = ref([]);
-    const error = ref(null);
-
-    const load = async() => {
-      try{
-        let data = await fetch('http://localhost:3000/posts') 
-        if(!data.ok){
-          throw Error('no data available')
+  setup() { 
+    const posts = ref([])
+    const error = ref(null)
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok) {
+          throw Error('no available data')
         }
         posts.value = await data.json()
         console.log(posts.value)
-      } catch(err) {
-        error.value = err.message; 
+      }
+      catch(err) {
+        error.value = err.message
+        console.log(error.value)
       }
     }
-
-  return { posts } 
+    load()
+    
+    return { posts, error }
   },
 }
 </script>
