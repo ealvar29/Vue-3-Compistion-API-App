4780d9fee0162a178d8b6b93cb453df15d79b6b1
Eduardo <ealvarez@carminaticonsulting.com>
2021-12-29 15:55:00 -0600

    single posts

--- dojo-blog/src/components/SinglePost.vue
+++ dojo-blog/src/components/SinglePost.vue
@@ -1,24 +1,27 @@
 <template>
-  <div>
-    <h2>{{ post.title }}</h2>
-    <p>{{ snippet }} </p>
+  <div class="post">
+    <router-link :to="{ name: 'Details', params: { id: post.id }}">
+     <h3>{{ post.title }}</h3>
+    </router-link>
+    <p>{{ snippet }}</p>
+    <span v-for="tag in post.tags" :key="tag">
+      #{{ tag }}
+    </span>
   </div>
 </template>
 
 <script>
-import { computed } from '@vue/reactivity'
-import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
+import { computed } from 'vue'
 export default {
-    name: 'SinglePost',
-    props: ['post'],
-    setup(props){
-        onMounted(() => console.log('component mounted'))
-        onUnmounted(() => console.log('component unmounted'))
-        onUpdated(() => console.log('component updated'))
-    }
+  props: ['post'],
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 100) + '...'
+    })
+    return { snippet }
+  }
 }
 </script>
 
 <style>
-
 </style>
