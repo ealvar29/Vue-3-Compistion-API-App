a4689a26ed5994c9da9bfbb11e18f3ea51467dd0
Eduardo <ealvarez@carminaticonsulting.com>
2022-01-02 15:06:53 -0600

    tag view

--- dojo-blog/src/router/index.js
+++ dojo-blog/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Details from '../views/Details.vue'
+import Create from '../views/Create.vue'
+import Tag from '../views/Tag.vue'
 
 const routes = [
   {
@@ -14,6 +16,16 @@ const routes = [
     component: Details,
     props: true
   },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create,
+  },
+  {
+    path: '/tags/:tag',
+    name: 'Tag',
+    component: Tag,
+  }
 ]
 
 const router = createRouter({
--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="home">
+    <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
     <div v-if="posts.length">
       <PostList :posts="posts" />
     </div>
-    <div v-else>Loading...</div>
+    <div v-else>
+      <Spinner />
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import getPosts from '../composables/getPosts'
 // component imports
 import PostList from '../components/PostList.vue'
-import getPosts from '../composables/getPosts'
-
+import Spinner from '../components/Spinner.vue'
 export default {
   name: 'Home',
-  components: { PostList },
+  components: { PostList, Spinner },
   setup() { 
     const { posts, error, load } = getPosts()
     load()
+    
     return { posts, error }
   },
 }
 </script>
+
+<style>
+  .home {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+</style>
